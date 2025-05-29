3f34c67d5853bdf92d74f711b933a7e104036b7d
iTheRedMist <brigadirchik1019@gmail.com>
2025-05-29 23:27:26 +0300

    lab9

---
+++ LAB9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LAB9
+{
+    internal class Program
+    {
+        public static void Task1()
+        {
+            int[] numbers = { 8, 5, 2, 9, 1, 5, 6 };
+            int swapCount = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                        swapCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Кількість перестановок: {swapCount}");
+            Console.WriteLine("Після сортування: ");
+            Console.Write(string.Join(", ", numbers));
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Task1();
+        }
+    }
+}
